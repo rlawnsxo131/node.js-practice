7d4f8bad1c93f40cac02d33685d7fd149eb86dd8
juntae <rlawnsxo131@gmail.com>
2019-01-03 23:54:39 +0900

    publish work(10.node-bird)20190103

--- 10.nodebrid/app.js
+++ 10.nodebrid/app.js
@@ -4,16 +4,18 @@ const morgan = require('morgan');
 const path = require('path');
 const session = require('express-session');
 const flash = require('connect-flash');
-const passport = require('passport'); //추가
+const passport = require('passport');
+const helmet = require('helmet');
+const hpp = require('hpp');
 require('dotenv').config();
 
 const pageRouter = require('./routes/page');
 const authRouter = require('./routes/auth');
-const postRouter = require('./routes/post');//추가
-const userRouter = require('./routes/user');//추가
+const postRouter = require('./routes/post');
+const userRouter = require('./routes/user');
 const { sequelize } = require('./models');
 const passportConfig = require('./passport');
-
+const logger = require('./logger');
 
 const app = express();
 sequelize.sync();
@@ -23,33 +25,47 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.set('port', process.env.PORT || 8001);
 
-app.use(morgan('dev'));
+if(process.env.NODE_ENV === 'production') {
+  app.use(morgan('combined'));
+  app.use(helmet());
+  app.use(hpp());
+} else {
+  app.use(morgan('dev'));
+}
+//app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/img', express.static(path.join(__dirname, 'uploads')));//추가
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
+const sessionOption = {
   resave: false,
   saveUninitialized: false,
   secret: process.env.COOKIE_SECRET,
   cookie: {
     httpOnly: true,
     secure: false,
-  },
-}));
+  }
+};
+if (process.env.NODE_ENV === 'production') {
+  sessionOption.proxy = true;
+  // sessionOption.cookie.secure = true;
+}
+app.use(session(sessionOption));
 app.use(flash());
 app.use(passport.initialize());//추가
 app.use(passport.session());//추가
 
 app.use('/', pageRouter);
 app.use('/auth', authRouter);
-app.use('/post', postRouter);//추가
-app.use('/user', userRouter);//추가
+app.use('/post', postRouter);
+app.use('/user', userRouter);
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
+  logger.info('hello');
+  logger.error(err.message);
   next(err);
 });
 
